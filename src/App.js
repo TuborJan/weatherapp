48bb9a7ef76d27ddf9f19b262e12b383d12024c9
Дмитрий Калита <TuborJan@yandex.ru>
2023-01-26 15:05:31 +0300

    feat: add styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import "./styles/App.scss";
+import { weatherRequest } from "./components/API/API";
 import WeatherScreen from "./components/WeatherScreen/WeatherScreen";
 
 function App() {
+  const [data, setData] = useState();
+  const [location, setLocation] = useState("");
+
+  const search = (event) => {
+    if (event.key === "Enter") {
+      weatherRequest(location, setData);
+    }
+  };
+
   return (
     <div className="App">
-      <WeatherScreen />
+      <input
+        className="input"
+        onChange={(event) => setLocation(event.target.value)}
+        onKeyDown={search}
+        placeholder="Enter your location"
+        type="text"
+      />
+      <WeatherScreen data={data} />
     </div>
   );
 }
--- src/components/WeatherScreen/WeatherScreen.jsx
+++ src/components/WeatherScreen/WeatherScreen.jsx
@@ -1,26 +1,54 @@
-import React, { useState } from "react";
-import { weatherRequest } from "../API/API";
+import React from "react";
+import "./WeatherScreen.scss";
 
-const WeatherScreen = () => {
-  const [data, setData] = useState({});
-  const [location, setLocation] = useState("");
-
-  const search = (event) => {
-    if (event.key === "Enter") {
-      weatherRequest(location, setData);
-    }
+const WeatherScreen = ({ data }) => {
+  const getData = (data) => {
+    let d = new Date(data * 1000);
+    let hh = d.getHours();
+    let min = ("0" + d.getMinutes()).slice(-2);
+    return `${hh}:${min}`;
   };
 
+  let url = "";
+
+  if (data) {
+    url = `./img/${data.weather[0].icon}.png`;
+  }
+
   return (
-    <div>
-      <h1>{data.name}</h1>
-      <p>{data.main.temp}</p>
-      <input
-        onChange={(event) => setLocation(event.target.value)}
-        onKeyDown={search}
-        placeholder="Enter your location"
-        type="text"
-      />
+    <div className="WeatherScreen">
+      {data ? (
+        <div className="container">
+          <div className="location">
+            <div className="img">
+              <img src={url} alt="weaher icon" />
+            </div>
+            <div className="city">{data.name}</div>
+            <div className="time">{`${getData(data.dt)}`}</div>
+          </div>
+
+          <div className="temperature">
+            {`${Math.round((data.main.temp - 273.15) * 10) / 10}`}
+            <span>°C</span>
+          </div>
+          <div className="main">{data.weather[0].main}</div>
+
+          <div className="weather">
+            <div className="feelsLike">
+              {`Fells like ${
+                Math.round((data.main.feels_like - 273.15) * 10) / 10
+              } C`}
+            </div>
+            <div className="wind">{`Wind ${data.wind.speed} m/h`}</div>
+            <div className="humidity">{`Humidity ${data.main.humidity}`}</div>
+            <div className="pressure">{`Pressure ${data.main.pressure}`}</div>
+          </div>
+        </div>
+      ) : (
+        <div className="alert">
+          <h1>Enter your location</h1>
+        </div>
+      )}
     </div>
   );
 };
